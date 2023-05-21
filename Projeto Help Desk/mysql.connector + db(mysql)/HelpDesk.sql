88830faa753ca7728ed1bf7ecc82baf5cc229696
VineDuarte <viniciusduarte18@gmail.com>
2023-05-21 20:48:43 -0300

    Update models - from db import  tables

--- Projeto Help Desk/mysql.connector + db(mysql)/HelpDesk.sql
+++ Projeto Help Desk/mysql.connector + db(mysql)/HelpDesk.sql
@@ -1,66 +1,45 @@
 Create database HDesk;
 use Hdesk;
 
-create table Pessoa( 
-IDPessoa int primary key, 
-NomePessoa varchar(80),
-Cpf_cnpj char(14),				
-Email varchar(80),
-Telefone varchar(10)
+create table Cliente(
+IDCliente int primary key auto_increment,
+NomeCliente varchar(80) not null,
+Cpf_cnpj int not null,				
+Email_Cliente varchar(80) not null,
+Telefone_Cliente varchar(10) not null,
+Assunto ENUM('NOSSOS APLICATIVOS','DÚVIDAS FINANCEIRAS','SUPORTE TÉCNICO', 'RECLAMAÇÕES', 'OUTRAS INFORMAÇÕES'),
+Descricao text(300)
 );
 
-create table Usuario (
-IDUsuario int not null,
-IDPessoa int not null, 
+create table Usuario(
+IDUsuario int primary key auto_increment, 
+NomeUsuario varchar(100) not null,
+Email_Usuario varchar(80) not null,
 Setor varchar(50),
 Turno varchar(1),
-Usuario_Ativo char(1),
-primary key (IDUsuario),
-foreign key(IDPessoa) references Pessoa(IDPessoa)
+Usuario_Ativo char(1)
 );
 
  create table Solicitacao(
- IDSolicitacao int primary key,
+ IDSolicitacao int primary key auto_increment,
+ IDCliente int,
  IDUsuario int, 
- IDTipo_Servico smallint,		 
- IDPrioridade smallint,
- Data_Solicitacao datetime,
- Descricao text,
+ Assunto ENUM('NOSSOS APLICATIVOS','DÚVIDAS FINANCEIRAS','SUPORTE TÉCNICO', 'RECLAMAÇÕES', 'OUTRAS INFORMAÇÕES'),		 
+ Prioridade ENUM('BAIXA', 'MÉDIA', 'ALTA'),
+ Data_Solicitacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
  SolicitacaoaAtivo Char(1),
- foreign key(IDUsuario) references Usuario(IDUsuario)
- );
- 
- create table SolicitacaoAnexo (
- IDSolicitacaoAnexo int primary key,
- IDSolicitacao int,
- NomeArquivoAnexo varchar(50),
- Path_ varchar(250),
- foreign key(IDSolicitacao) references Solicitacao(IDSolicitacao)
- );
- 
- create table Status_ (
- IDStatus smallint primary key, 
- NomeStatus varchar(20),
- Status_Ativo char(1)
+ foreign key(IDUsuario) references Usuario(IDUsuario),
+ foreign key(IDCliente) references Cliente(IDCliente)
  );
  
  create table SolicitacaoStatus(
- IDSolicitacaoStatus int primary key, 
+ IDSolicitacaoStatus int primary key auto_increment,
  IDSolicitacao int, 
- IDStatus smallint, 					
+ IDStatus ENUM('ABERTO','FECHADO', 'EM ANDAMENTO'),
  IDUsuario int,
- DataStatus Datetime,
+ DataStatus Datetime, 														
  foreign key(IDUsuario) references Usuario(IDUsuario),
- foreign key(IDSolicitacao) references Solicitacao(IDSolicitacao),
- foreign key(IDStatus) references Status_(IDStatus)
- );
- 
- create table Prioridade (
- IDPrioridade smallint primary key,
- IDSolicitacao int,
- NomePrioridade varchar(20),
- PrioridadeAtivo char(1),
- foreign key(IDSolicitacao ) references Solicitacao(IDSolicitacao)
+ foreign key(IDSolicitacao) references Solicitacao(IDSolicitacao)
  );
 
     SELECT * FROM atendimento;
