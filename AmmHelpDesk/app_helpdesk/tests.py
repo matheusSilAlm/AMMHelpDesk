15ce5b517f60a8da5286d84bb8c1effac6a71fe0
VineDuarte <viniciusduarte18@gmail.com>
2023-07-09 23:35:51 -0300

    Rapaz 99,93% pronto

--- AmmHelpDesk/app_helpdesk/tests.py
+++ AmmHelpDesk/app_helpdesk/tests.py
@@ -1,22 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
 if request.method == 'POST':
         cliente = Cliente.objects.get(idcliente=idcliente)
+        resposta_usuario = request.POST.get('resposta_usuario')
         cliente.resposta_usuario = resposta_usuario
+
+        # Renderizar o template do e-mail
+        context = {
+            'cliente': cliente,
+            'resposta_usuario': resposta_usuario,
+        }
+        html_message = render_to_string('pageclienteX.html', context)
+        text_message = strip_tags(html_message)
+
+        # Anexar a imagem embutida ao e-mail
+        image_path = os.path.join(settings.BASE_DIR, 'static', 'img', 'AmmLOGO.webp')
+        with open(image_path, 'rb') as f:
+            image_data = f.read()
+        image = MIMEImage(image_data)
+        image.add_header('Content-ID', '<logo_image>')
+        image.add_header('Content-Disposition', 'inline', filename='AmmLOGO.webp')
+
+        # Atualizar o conteúdo do e-mail para incluir a imagem embutida
+        html_message = html_message.replace('src="logo_image"', f'src="cid:logo_image"')
+
+        # Enviar e-mail para o cliente
+        subject = f'Resposta ao seu chamado: {cliente.assunto}' 
+        from_email = 'teushiftz@gmail.com'  
+        to_email = cliente.email_cliente  
+
+        email = EmailMultiAlternatives(subject, text_message, from_email, [to_email])
+        email.attach_alternative(html_message, "text/html")
+
+        # Processar o anexo enviado pelo usuário, se existir
+        anexo = request.FILES.get('anexo')
+        if anexo:
+            email.attach(anexo.name, anexo.read(), anexo.content_type)
+
+        email.send()
         cliente.save()
-        
-        if resposta_usuario == True:
-            resposta_usuario = form.cleaned_data['resposta_usuario']
-            
-            soup = BeautifulSoup(resposta_usuario, 'html.parser')
-            resposta_texto = strip_tags(str(soup))
-
-
-            subject = f'Resposta ao seu chamado: {cliente.assunto}'
-            message = f'Olá {cliente.nomecliente},\n\nSua solicitação foi respondida.\n\nSua descrição do chamado: {cliente.descricao}\n\n\n\nResposta: {resposta_texto}\n\nAtenciosamente,\nEquipe de suporte'
-            email = EmailMultiAlternatives(subject, message, from_email, [to_email])
-            email.send()
-    
-        
         return redirect('/')
--- AmmHelpDesk/app_helpdesk/views.py
+++ AmmHelpDesk/app_helpdesk/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout 
@@ -15,8 +16,9 @@ from django.template.loader import render_to_string
 from django.conf import settings
 from bs4 import BeautifulSoup
 from django.templatetags.static import static
-import os
 from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
 
 
 # Usuário faz login na pagina.
@@ -113,6 +115,7 @@ def update_cliente(request, idcliente):
         
         return redirect('/')
     
+    
     if request.method == 'POST':
         cliente = Cliente.objects.get(idcliente=idcliente)
         resposta_usuario = request.POST.get('resposta_usuario')
@@ -127,24 +130,29 @@ def update_cliente(request, idcliente):
         text_message = strip_tags(html_message)
 
         # Anexar a imagem embutida ao e-mail
-        image_path = os.path.join(settings.BASE_DIR, 'static', 'img', 'AmmLOGO.webp')
+        image_path = os.path.join(settings.BASE_DIR, 'static', 'img', 'amm-navbar-fnt.png')
         with open(image_path, 'rb') as f:
             image_data = f.read()
         image = MIMEImage(image_data)
         image.add_header('Content-ID', '<logo_image>')
-        image.add_header('Content-Disposition', 'inline', filename='AmmLOGO.webp')
+        image.add_header('Content-Disposition', 'inline', filename='amm-navbar-fnt.png')
 
         # Atualizar o conteúdo do e-mail para incluir a imagem embutida
         html_message = html_message.replace('src="logo_image"', f'src="cid:logo_image"')
 
         # Enviar e-mail para o cliente
         subject = f'Resposta ao seu chamado: {cliente.assunto}' 
-        from_email = 'teushiftz@gmail.com'  # E-mail remetente
-        to_email = cliente.email_cliente  # E-mail do cliente
+        from_email = 'teushiftz@gmail.com'  
+        to_email = cliente.email_cliente  
 
         email = EmailMultiAlternatives(subject, text_message, from_email, [to_email])
         email.attach_alternative(html_message, "text/html")
         email.attach(image)
+
+        # Processar o anexo enviado pelo usuário, se existir
+        anexo = request.FILES.get('anexo')
+        if anexo:
+            email.attach(anexo.name, anexo.read(), anexo.content_type)
 
         email.send()
         cliente.save()
